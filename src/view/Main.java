1fb129dbf21f920de0bd3a8f569f91ac4642bebc
DEICIDIA <thoum.nathan@gmail.com>
2020-01-23 14:05:09 +0100

    test

---
+++ src/view/Main.java
@@ -0,0 +1,10 @@
+package view;
+
+public class Main {
+
+    public static void main(String[] args) {
+        test t1 = new test();
+        t1.setVisible(true);
+    }
+
+}
---
+++ src/view/test.java
@@ -0,0 +1,19 @@
+package view;
+
+import javax.swing.*;
+
+
+public class test extends JFrame {
+    
+    private JPanel rootPanel;
+    private JButton test;
+    private JPanel panel1;
+
+    public test(){
+        add(rootPanel);
+        setTitle("pute pute pute");
+        setSize(400,500);
+
+    }
+
+}
