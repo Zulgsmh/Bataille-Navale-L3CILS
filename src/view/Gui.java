4f90a2e7a288c5052c102b3581065f8c91710bde
DEICIDIA <thoum.nathan@gmail.com>
2020-01-23 16:10:03 +0100

    PREMIER MENU

--- src/view/Gui.java
+++ src/view/Gui.java
@@ -1,18 +1,65 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 
 public class Gui extends JFrame {
-    
+    Color purple = new Color(110,74,227);
     private JPanel rootPanel;
-    private JButton test;
+    private JButton DEMO;
+    private JButton UN;
+    private JButton DEUX;
 
     public Gui(){
         add(rootPanel);
-        setTitle("pute pute pute");
-        setSize(400,500);
+        setTitle("BATAILLE NAVALE");
+        setSize(800,500);
 
+        UN.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                UN.setBackground(Color.white);
+            }
+        });
+        UN.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+                UN.setBackground(purple);
+            }
+        });
+        DEUX.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                DEUX.setBackground(Color.white);
+            }
+        });
+        DEUX.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+                DEUX.setBackground(purple);
+            }
+        });
+        DEMO.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                DEMO.setBackground(Color.white);
+            }
+        });
+        DEMO.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+                DEMO.setBackground(purple);
+            }
+        });
     }
 
 }
